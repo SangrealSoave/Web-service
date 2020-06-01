89621df6236978ca081aabd40ccef38ef7eca9da
SangrealSoave <48495284+SangrealSoave@users.noreply.github.com>
2020-06-01 13:14:45 +0300

    Add files via upload

---
+++ MscJobsForDisabled.WebService/ApplicationServices/GetForecastListUseCase/GetJobListUseCaseRequest.cs
@@ -0,0 +1,28 @@
+﻿using MscJobsForDisabled.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MscJobsForDisabled.ApplicationServices.GetJobListUseCase
+{
+    public class GetJobListUseCaseRequest : IUseCaseRequest<GetJobListUseCaseResponse>
+    {
+        public long? JobId { get; private set; }
+
+        private GetJobListUseCaseRequest()
+        { }
+
+        public static GetJobListUseCaseRequest CreateAllJobsRequest()
+        {
+            return new GetJobListUseCaseRequest();
+        }
+
+        public static GetJobListUseCaseRequest CreateJobRequest(long jobId)
+        {
+            return new GetJobListUseCaseRequest() { JobId = jobId };
+
+        }
+    }
+}
+
---
+++ MscJobsForDisabled.WebService/ApplicationServices/GetForecastListUseCase/GetJobListUseCaseResponse.cs
@@ -0,0 +1,16 @@
+﻿using MscJobsForDisabled.DomainObjects;
+using MscJobsForDisabled.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MscJobsForDisabled.ApplicationServices.GetJobListUseCase
+{
+    public class GetJobListUseCaseResponse : UseCaseResponse
+    {
+        public IEnumerable<Job> Jobs { get; }
+
+        public GetJobListUseCaseResponse(IEnumerable<Job> jobs) => Jobs = jobs;
+    }
+}
