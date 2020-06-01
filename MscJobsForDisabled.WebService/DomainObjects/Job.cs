956b497fd6358f2cf9ed91f0da7184bf0113f3e4
SangrealSoave <48495284+SangrealSoave@users.noreply.github.com>
2020-06-01 13:13:23 +0300

    Add files via upload

---
+++ MscJobsForDisabled.WebService/DomainObjects/Job.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MscJobsForDisabled.DomainObjects
+{
+    public class Job : DomainObject  
+    {
+        public string Name { get; set; }
+
+        public string Specialization { get; set; }
+
+        public string WorkingHours { get; set; }
+
+        public string Location { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
